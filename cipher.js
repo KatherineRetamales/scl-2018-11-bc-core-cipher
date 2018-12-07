e365405ba141d0e74228058bb8e48958de64617d
katherine <kretamales280@gmail.com>
2018-12-07 03:59:00 -0300

    Se hizo parte del hacker edition

--- cipher.js
+++ cipher.js
@@ -1,29 +1,77 @@
 window.cipher = {
-  encode: (frase,desplazamiento) => {
-    let newFrase = '';
-    let offSetAscii = 0 ;
-    for(let i = 0; i < frase.length; i++){
+  //funcion cifrar
+  encode: (frase, desplazamiento) => {
+    let newFrase = ''; //se declara la variable donde se guardara el texto cifrado
+    let offSetAscii = 0; //se declara la variable donde se guardara la suma del numero ascii mas el desplazamiento
+    for (let i = 0; i < frase.length; i++) {
       let ascii = frase.charCodeAt(i);
+      // condicion para el espacio
       if (ascii === 32) {
-          offSetAscii = ascii ;
-      }else {
-          offSetAscii = (ascii - 65 + desplazamiento) % 26 + 65 ;
+        offSetAscii = ascii;
       }
-      newFrase +=String.fromCharCode(offSetAscii);
+      //condicion para la letra ñ minuscula
+      if (ascii === 241) {
+        offSetAscii = ascii;
+      }
+      //condicion para la letra Ñ mayuscula
+      if (ascii === 209) {
+        offSetAscii = ascii;
+      }
+      //condicion para letras mayusculas
+      if (ascii >= 65 && ascii <= 90) {
+        offSetAscii = (ascii - 65 + desplazamiento) % 26 + 65;
+      }
+      //condicion para letras minusculas
+      if (ascii >= 97 && ascii <= 122) {
+        offSetAscii = (ascii - 97 + desplazamiento) % 26 + 97;
+      }
+      //condicion para caracter especiales
+      if (ascii >= 33 && ascii <= 47) {
+        offSetAscii = (ascii - 33 + desplazamiento) % 15 + 33;
+      }
+      //condicion para numeros
+      if (ascii >= 48 && ascii <= 57) {
+        offSetAscii = (ascii - 48 + desplazamiento) % 9 + 48;
+      }
+      newFrase += String.fromCharCode(offSetAscii);
     }
     return newFrase;
   },
-  decode: (frase,desplazamiento) => {
-    let newFrase = '';
-    let offSetAscii = 0 ;
-    for(let i = 0; i < frase.length; i++){
+
+  //funcion descifrar
+  decode: (frase, desplazamiento) => {
+    let newFrase = ''; //se declara la variable donde se guardara el texto descifrado
+    let offSetAscii = 0;
+    for (let i = 0; i < frase.length; i++) {
       let ascii = frase.charCodeAt(i);
+      //condicion para el espacio
       if (ascii === 32) {
-          offSetAscii = ascii ;
-      }else{
-          offSetAscii = (ascii + 65 - desplazamiento) % 26 + 65 ;
+        offSetAscii = ascii;
+      } //condicion para la letra ñ minuscula
+      if (ascii === 241) {
+        offSetAscii = ascii;
       }
-      newFrase +=String.fromCharCode(offSetAscii);
+      //condicion para la letra Ñ mayuscula
+      if (ascii === 209) {
+        offSetAscii = ascii;
+      }
+      //condicion para letras mayusculas
+      if (ascii >= 65 && ascii <= 90) {
+        offSetAscii = (ascii - 90 - desplazamiento) % 26 + 90;
+      }
+      //condicion para letras minusculas
+      if (ascii >= 97 && ascii <= 122) {
+        offSetAscii = (ascii - 122 - desplazamiento) % 26 + 122;
+      }
+      //condicion para caracter especiales
+      if (ascii >= 33 && ascii <= 47) {
+        offSetAscii = (ascii - 47 - desplazamiento) % 15 + 47;
+      }
+      //condicion para numeros
+      if (ascii >= 48 && ascii <= 57) {
+        offSetAscii = (ascii - 57 - desplazamiento) % 9 + 57;
+      }
+      newFrase += String.fromCharCode(offSetAscii);
     }
     return newFrase;
   }
--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-window.onload =() => {
+window.onload = () => {
   document.getElementById('btnCifrar').addEventListener('click',
   (evento)=>{
     evento.preventDefault();
-    let frase = document.getElementById('txtTexto').value.toUpperCase(); //Capturar el texto y se guarda en una variable
-    let desplazamiento = parseInt(document.getElementById('txtDesplazamiento').value);
+    let frase = document.getElementById('txtTexto').value; //Capturar el texto y se guarda en una variable
+    let desplazamiento = parseInt(document.getElementById('txtDesplazamiento').value);// capturo el desplazamiento y lo guardo en una variable
     let fraseCifrada = cipher.encode(frase,desplazamiento);
     document.getElementById('txtdecode').innerHTML=fraseCifrada; // Se muetra en texto descifrado
   })
+
   document.getElementById('btnDescifrar').addEventListener('click',
   (evento)=>{
     evento.preventDefault();
-    let frase = document.getElementById('txtTexto').value.toUpperCase();
+    let frase = document.getElementById('txtTexto').value;
     let desplazamiento = parseInt(document.getElementById('txtDesplazamiento').value);
     let fraseDescifrada = cipher.decode(frase,desplazamiento);
     //.log(fraseDescifrada)
     document.getElementById('txtdecode').innerHTML=fraseDescifrada;
+  })
+  document.getElementById('btnReset').addEventListener('click',
+  (evento)=>{
+    evento.preventDefault();
+    let borrarTexto = document.getElementById('txtTexto');
+    if(borrarTexto){
+      borrarTexto.value = "";
+    }
+  }) 
+  //boton para copiar el mensaje de resultado
+  document.getElementById("btnCopiar").addEventListener('click',
+  (evento) => {
+    evento.preventDefault();
+    let copyText = document.getElementById("txtdecode");
+    copyText.select();
+    document.execCommand("copy");
   })
 }
